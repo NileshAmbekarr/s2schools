35c2f5e3eb6d4f3d19dd33cf830d22b392a0c7b7
Nilesh Raju Ambekar <nileshambekar9281@gmail.com>
2025-06-18 21:30:40 +0530

    Update index.js

--- src/data/tutorials/html/index.js
+++ src/data/tutorials/html/index.js
@@ -13,5 +13,71 @@ export { htmlComments } from './comments';
 export { htmlColors } from './colors';
 export { htmlCss } from './css';
 export { htmlLinks } from './links';
+export { htmlImages } from './images';
+export { htmlFavicon } from './favicon';
+export { htmlPageTitle } from './page_title';
+export { htmlTables } from './tables';
+export { htmlLists } from './lists';
+export { htmlBlockInline } from './block_inline';
+export { htmlDiv } from './div';
+export { htmlClasses } from './classes';
+export { htmlId } from './id';
+export { htmlIframes } from './iframes';
+export { htmlJavaScript } from './javascript';
+export { htmlFilePaths } from './file_paths';
+export { htmlHead } from './head';
+export { htmlLayout } from './layout';
+export { htmlResponsive } from './responsive';
+export { htmlComputercode } from './computercode';
+export { htmlSemantics } from './semantics';
+export { htmlStyleGuide } from './style_guide';
+export { htmlEntities } from './entities';
+export { htmlSymbols } from './symbols';
+export { htmlEmojis } from './emojis';
+export { htmlCharsets } from './charsets';
+export { htmlUrlEncode } from './url_encode';
+export { htmlVsXhtml } from './vs_xhtml';
 
 // As we add more tutorial files, we'll export them here 
+/* 
+            HTML HOME
+            HTML Introduction
+            HTML Editors
+            HTML Basic
+            HTML Elements
+            HTML Attributes
+            HTML Headings
+            HTML Paragraphs
+            HTML Styles
+            HTML Formatting
+            HTML Quotations
+            HTML Comments
+            HTML Colors
+            HTML CSS
+            HTML Links
+            HTML Images
+            HTML Favicon
+            HTML Page Title
+            HTML Tables
+            HTML Lists
+            HTML Block & Inline
+            HTML Div
+            HTML Classes
+            HTML Id
+            HTML Iframes
+            HTML JavaScript
+            HTML File Paths
+            HTML Head
+            HTML Layout
+            HTML Responsive
+            HTML Computercode
+            HTML Semantics
+            HTML Style Guide
+            HTML Entities
+            HTML Symbols
+            HTML Emojis
+            HTML Charsets
+            HTML URL Encode
+            HTML vs. XHTML
+
+*/
